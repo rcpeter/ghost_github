3affeef66abe2f562e6fac6ef6ecbee2d3d7bb3c
Ronnit Peter <ronnit@Ronnits-MacBook-Air.local>
2024-12-20 16:33:07 -0600

    adding script files to commit

--- randomize_cron.sh
+++ randomize_cron.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Generate random times
-for i in {1..4} do
-  HOUR=$((RANDOM % 24))
-  MINUTE=$((RANDOM % 60))
-  echo "$MINUTE $HOUR * * * /Users/ronnit/Documents/Coding/ghost_github/random_commit.sh"
+for i in {1..4}; do
+    HOUR=$((RANDOM % 24))
+    MINUTE=$((RANDOM % 60))
+    echo "$MINUTE $HOUR * * * /Users/ronnit/Documents/Coding/ghost_github/random_commit.sh"
 done > random_cron.tmp
 
 # Install the new crontab
